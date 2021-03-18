dd2209f533a9175002bf5eef07cdbfe150824167
SovietSpy2 <sovietspy2@users.noreply.github.com>
2021-03-18 14:08:21 +0100

    root endpoint log

--- src/main/java/com/example/azuredemo/WelcomeController.java
+++ src/main/java/com/example/azuredemo/WelcomeController.java
@@ -1,9 +1,11 @@
 package com.example.azuredemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class WelcomeController {
 
@@ -12,6 +14,7 @@ public class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
+        log.info(" root endpoint accessed");
         return "my enviorment propery:"+value;
     }
 
